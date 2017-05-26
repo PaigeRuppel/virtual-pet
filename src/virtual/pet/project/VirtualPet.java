8f8fceabd20fd6c92a7ba20f3cc37ca1dd9178aa
Paige Ruppel <paige.ruppel@gmail.com>
2017-05-26 12:08:05 -0400

    first draft

--- src/virtual/pet/project/VirtualPet.java
+++ src/virtual/pet/project/VirtualPet.java
@@ -2,4 +2,43 @@ package virtual.pet.project;
 
 public class VirtualPet {
 
+	int hunger = 70;
+	int boredom = 70;
+	int tired = 70;
+
+	void tick() {
+		hunger = hunger + 5;
+		boredom = boredom + 5;
+		tired = tired + 5;
+	}
+
+	void giveFood() {
+		hunger = hunger - 20;
+	}
+
+	boolean isHungry() {
+		return hunger >= 60;
+	}
+
+	void playWith() {
+		boredom = boredom - 20;
+	}
+
+	boolean isBored() {
+		return boredom >= 60;
+	}
+
+	void giveRest() {
+		tired = tired - 20;
+	}
+
+	boolean isTired() {
+		return tired >= 60;
+	}
+
+	boolean running() {
+		return true;
+	}
+
+	// void feed(int amount) { }
 }
--- src/virtual/pet/project/VirtualPetApp.java
+++ src/virtual/pet/project/VirtualPetApp.java
@@ -1,13 +1,54 @@
 package virtual.pet.project;
 
+import java.util.Scanner;
+
 public class VirtualPetApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	
-	// game loop that calls tick() to advance time - calling tick() will increase
-		// hunger, thirst, boredom, etc.
+		Scanner input = new Scanner(System.in);
+
+		VirtualPet bob = new VirtualPet();
+
+		System.out.println("This is your new pet Bob! He has basic needs like food, activity, and rest.");
+		System.out.println("Here is what you can do with Bob:");
+		System.out.println(
+				"To check on the needs of Bob, type \"needs?\" - this will give you a summary of Bob's current state.");
+		System.out.println(" - type \"give food\" - this will decrease Bob's hunger by 20.");
+		System.out.println(" - type \"play with\" - this will decrease Bob's boredom by 20.");
+		System.out.println(" - type \"give rest\" - this will decrease Bob's boredom by 20.");
+		System.out.println();
+		System.out.println("Are you ready to care for Bob? Type \"yes\" to begin the game.");
+		String begin = input.nextLine();
+
+		if (begin.toLowerCase().equals("yes")) {
+			System.out.println("Bob now depends on you for life.");
+		} else {
+			System.out.println("Goodbye.");
+			System.exit(0);
+		}
+
+		do {
+			bob.tick();
+			System.out.println("What would you like to do with Bob?");
+			String command = input.nextLine();
+			switch (command.toLowerCase()) {
+			case ("give food"):
+				bob.giveFood();
+				break;
+			case ("play with"):
+				bob.playWith();
+				break;
+			case ("give rest"):
+				bob.giveRest();
+				break;
+			case ("needs?"):
+				System.out.println("bored - " + bob.isBored());
+				System.out.println("hungry - " + bob.isHungry());
+				System.out.println("tired - " + bob.isTired());
+				break;
+			}
+		} while (bob.running());
 	}
 
 }
