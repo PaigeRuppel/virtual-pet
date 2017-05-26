c907bce13026ba07e7e96f427dd91c83e0178343
Paige Ruppel <paige.ruppel@gmail.com>
2017-05-26 09:52:05 -0400

    setup

--- src/virtual/pet/project/VirtualPetApp.java
+++ src/virtual/pet/project/VirtualPetApp.java
@@ -5,6 +5,9 @@ public class VirtualPetApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	
+	// game loop that calls tick() to advance time - calling tick() will increase
+		// hunger, thirst, boredom, etc.
 	}
 
 }
